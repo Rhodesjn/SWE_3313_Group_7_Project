d88a574a8459c30217e73742263222572623de99
Rhodesjn <rhodes.justin15@gmail.com>
2025-04-26 23:29:29 -0400

    The testing class  is now functional

--- Test.java
+++ Test.java
@@ -1,60 +1,46 @@
 import java.util.Scanner;
 import java.util.Date;
-import java.util.NoSuchElementException;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 public class Test {
    public static void main(String[] args) throws ParseException{
+      Scanner scan = new Scanner(System.in);
       //creating User 
-      NewUser user = creatingUser();
-  
-      Income userIncome = inputingIncom(user);
+      NewUser user = creatingUser(scan);
+      //adding income for the user
+      Income userIncome = inputingIncom(user, scan);
       user.addRecord(userIncome);
-      //testing userIncome's getters
+
+      //printing user's information 
       for (FinancialRecord record : user.getRecords()){
          System.out.println("Record for "+record.getUser().getName() + " : $"+record.getAmount());
       }
+      scan.close();
    }
 
-   public static NewUser creatingUser(){
-      Scanner scan = new Scanner(System.in);
+   public static NewUser creatingUser(Scanner scan){
+      
       System.out.println("What is your name?");
       String userName = scan.nextLine();
       System.out.println("What is your email?");
       String userEmail = scan.nextLine();
       System.out.println("What currency do you use?");
       String userCurrnecy = scan.nextLine();
-      scan.close();
       return new NewUser(userName, userEmail, userCurrnecy);
    }
-   
-    public static Income inputingIncom (NewUser user) throws ParseException {
-      Scanner scan = new Scanner(System.in);
+    public static Income inputingIncom (NewUser user, Scanner scan) throws ParseException {
       System.out.println("What is the total amount of $$ in your account?");
-      double userAmount = 0;
-      try{
-         userAmount=scan.nextDouble();
-      } catch(NoSuchElementException e){
-         System.out.println("No valid input found. Please Try again.");
-      }
-   
-
-      //scan.nextLine();      
-
-      //scan.nextLine();
+      double userAmount = scan.nextDouble();
+      scan.nextLine();
       System.out.println("Enter the date of Trasaction (MM/dd/yyyy): ");
-      String dateString="";
-      if(scan.hasNextLine()){
-         dateString = scan.nextLine();
-      }
+      String dateString=scan.nextLine();
       SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
       dateFormat.setLenient(false);
       Date date = dateFormat.parse(dateString);
       scan.close();
       return new Income(userAmount, date, user);
    }
-
 }
 
 
